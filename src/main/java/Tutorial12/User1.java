437eb9722a0040ea617b36d2009348156843b0ca
lenovo <cs.17.205@student.uotechnology.edu.iq>
2021-02-07 13:27:21 +0300

    Table and Database

---
+++ src/main/java/Tutorial12/User1.java
@@ -0,0 +1,82 @@
+package Tutorial12;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+public class User1 {
+
+    private final SimpleObjectProperty id;
+    private final SimpleObjectProperty firstName;
+    private final SimpleObjectProperty lastName;
+    private final SimpleObjectProperty email;
+    private final SimpleObjectProperty userName;
+    private final SimpleObjectProperty password;
+    private final SimpleObjectProperty DOB;
+
+    public User1(int id, String firstName, String lastName, String email, String userName, String password, String DOB) {
+        this.id = new SimpleObjectProperty(id);
+        this.firstName = new SimpleObjectProperty(firstName);
+        this.lastName = new SimpleObjectProperty(lastName);
+        this.email = new SimpleObjectProperty(email);
+        this.userName = new SimpleObjectProperty(userName);
+        this.password = new SimpleObjectProperty(password);
+        this.DOB = new SimpleObjectProperty(DOB);
+    }
+
+
+    public Object getId() {
+        return id.get();
+    }
+
+    public Object getFirstName() {
+        return firstName.get();
+    }
+
+    public Object getLastName() {
+        return lastName.get();
+    }
+
+    public Object getEmail() {
+        return email.get();
+    }
+
+    public Object getUserName() {
+        return userName.get();
+    }
+
+    public Object getPassword() {
+        return password.get();
+    }
+
+    public Object getDOB() {
+        return DOB.get();
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public void setUserName(String userName) {
+        this.userName.set(userName);
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB.set(DOB);
+    }
+}
