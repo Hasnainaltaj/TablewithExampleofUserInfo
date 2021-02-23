12721da448507c6ac2e3bd3c98d49ab8b103d9a1
lenovo <cs.17.205@student.uotechnology.edu.iq>
2021-02-23 13:06:43 +0300

    links

---
+++ src/main/java/Tutorial12/CenteringText.java
@@ -0,0 +1,13 @@
+package Tutorial12;
+
+public class CenteringText {
+
+
+    //http://www-acad.sheridanc.on.ca/~jollymor/prog24178/javafx_mvc1.html
+    //https://download.code-projects.org/details/59ea954e-e3c2-4296-a16f-852af8fbdbcd
+    //https://openjfx.io/
+    //https://www.swtestacademy.com/database-operations-javafx/
+    //https://www.chegg.com/homework-help/questions-and-answers/sample-javafx-app-jdbc-file-operations-help-employee-id-new-email-name-surname-email-name--q32702649#question-transcript
+    //https://www.codersarts.com/post/employee-management-system-using-javafx
+    //https://medium.com/@keeptoo/javafx-java-modern-ui-design-starter-pack-aab1c331fd3c
+}
